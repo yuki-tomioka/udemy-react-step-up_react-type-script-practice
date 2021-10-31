1b1286460ae6d870414d50a993711fb185a6aae0
Yuki Tomioka <yuki.tomioka.1016@gmail.com>
2021-10-31 18:26:19 +0900

    型定義がない状態でバグが出てる例

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,21 @@
 import axios from "axios";
+import { useState } from "react";
 import "./styles.css";
+import { Todo } from "./Todo";
 
 export default function App() {
+  const [todos, setTotos] = useState<any>([]);
   const onClickFetchData = () => {
     axios.get("https://jsonplaceholder.typicode.com/todos").then((res) => {
-      console.log(res);
+      setTotos(res.data);
     });
   };
   return (
     <div className="App">
       <button onClick={onClickFetchData}>データ取得</button>
+      {todos.map((todo) => (
+        <Todo title={todo.title} userid={todo.userid} />
+      ))}
     </div>
   );
 }
