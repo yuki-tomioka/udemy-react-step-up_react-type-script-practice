d5226cefa67c1df1aaa0ea3772c14a540c51ed98
Yuki Tomioka <yuki.tomioka.1016@gmail.com>
2021-10-31 18:57:31 +0900

    Omitによる型指定

--- src/Todo.tsx
+++ src/Todo.tsx
@@ -1,8 +1,6 @@
 import { TodoType } from "./types/todo";
 
-export const Todo = (
-  props: Pick<TodoType, "title" | "userId" | "completed">
-) => {
+export const Todo = (props: Omit<TodoType, "id">) => {
   const { title, userId, completed = false } = props;
   const completeMark = completed ? "[完]" : "[未]";
   return <p>{`${completeMark} ${title}(ユーザー: ${userId})`}</p>;
