949fbc274ae6f759e7fa28bc9f05c605c3eee6c3
Yuki Tomioka <yuki.tomioka.1016@gmail.com>
2021-10-31 18:56:37 +0900

    Pickによる型指定

--- src/Todo.tsx
+++ src/Todo.tsx
@@ -1,10 +1,8 @@
-type TodoType = {
-  userId: number;
-  title: string;
-  completed?: boolean;
-};
+import { TodoType } from "./types/todo";
 
-export const Todo = (props: TodoType) => {
+export const Todo = (
+  props: Pick<TodoType, "title" | "userId" | "completed">
+) => {
   const { title, userId, completed = false } = props;
   const completeMark = completed ? "[完]" : "[未]";
   return <p>{`${completeMark} ${title}(ユーザー: ${userId})`}</p>;
